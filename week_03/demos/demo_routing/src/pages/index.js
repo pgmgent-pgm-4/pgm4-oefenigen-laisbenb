5c700eca26f3481b49f9af4c1240b617a2342379
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-04-29 11:10:54 +0200

    Week 3 demo router

---
+++ week_03/demos/demo_routing/src/pages/index.js
@@ -0,0 +1,8 @@
+import Contact from "./Contact";
+import Home from "./Home";
+import About from "./About";
+import NotFound from "./NotFound";
+import Blog from "./Blog";
+import BlogPost from "./BlogPost";
+
+export { About, Home, Contact, NotFound, Blog, BlogPost }
